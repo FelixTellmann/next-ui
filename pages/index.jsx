1079afb65a1cc39c1309e91dbf900a3bf771bccf
FelixTellmann <felix@tellmann.co.za>
2020-09-08 19:41:43 +0200

    Added storybook

--- pages/index.jsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-  return <>
-      <h1>Welcome to Next.js</h1>
-  </>
-}
-
-export default Home;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Badge } from "components/Badge";
+
+const Home = () => {
+  return <>
+    <h1>Welcome to Next.js</h1>
+    <Badge status="positive">Positive</Badge>
+    <Badge status="warning">Warning</Badge>
+  </>;
+};
+
+export default Home;
