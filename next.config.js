9c5ec789207ad43f685e95c5fae5dcc7b2adcd6e
FelixTellmann <felix@tellmann.co.za>
2020-09-15 17:07:41 +0200

    Start with Web Dev for Felixtellmann.com
    Added:
    - Heading.tsx
    - Paragraph.tsx
    - NavButton.tsx
    Dark mode
    Header.tsx
    Footer.tsx
    
    Busy on Index Page
    ADded
    - Timeline
    
    ADded svg imports & next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,48 +1,58 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-const SCSS = require('@zeit/next-sass');
-const path = require('path');
-const fs = require('fs');
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+const SCSS = require("@zeit/next-sass");
+const path = require("path");
+const fs = require("fs");
 
-module.exports = withPlugins([
-  [optimizedImages, {
-    optimizeImagesInDev: true,
-    /* config for next-optimized-images */
-  }],
-  [SCSS],
-], {
-/*  entry: './pages/index.tsx',
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        optimizeImagesInDev: true,
+        /* config for next-optimized-images */
+      },
+    ],
+    [SCSS],
+  ],
+  {
+    /*  entry: './pages/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist/snippets'),
     filename: 'my-first-webpack.bundle.js'
   },*/
-  webpack(config, { defaultLoaders }) {
-
-    config.module.rules.push({
+    webpack(config, { defaultLoaders }) {
+      config.module.rules.push(
+        {
           test: /\.(png|eot|otf|ttf|woff|woff2)$/,
           use: {
-            loader: 'url-loader',
+            loader: "url-loader",
           },
         },
         {
-          loader: 'sass-loader',
+          loader: "sass-loader",
           test: /.scss$/,
           options: {
             sassOptions: {
-              outputStyle: 'expanded',
+              outputStyle: "expanded",
               sourceMap: true,
             },
           },
         },
         {
-          enforce: 'pre',
+          enforce: "pre",
           test: /.scss$/,
-          loader: 'sass-resources-loader',
+          loader: "sass-resources-loader",
           options: {
-            resources: fs.readdirSync(path.join(process.cwd(), 'styles')).filter(file => file.match(/^_.*\.scss$/)).map(file => './styles/' + file),
+            resources: fs
+              .readdirSync(path.join(process.cwd(), "styles"))
+              .filter((file) => file.match(/^_.*\.scss$/))
+              .map((file) => "./styles/" + file),
           },
-        });
-    config.resolve.extensions = [".ts", ".js", ".jsx", ".tsx", ".svg"];
-    return config;
-  },
-});
+        }
+      );
+      config.resolve.extensions = [".ts", ".js", ".jsx", ".tsx", ".svg", ".scss", "*.mdx"];
+      return config;
+    },
+  }
+);
