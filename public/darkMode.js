ee27ad7309d4d1689c0e8d704588ea7632c49de5
FelixTellmann <felix@tellmann.co.za>
2020-09-22 12:25:20 +0200

    Start with Web Dev for Felixtellmann.com
    Added:
    - Heading.tsx
    - Paragraph.tsx
    - NavButton.tsx
    Dark mode
    Header.tsx
    Footer.tsx
    
    Busy on Index Page
    ADded
    - Timeline
    
    ADded svg imports & next-optimized-images
    
    Added Blog Support

--- public/darkMode.js
+++ public/darkMode.js
@@ -7,8 +7,6 @@
   var classNameDark = 'dark-mode';
   var classNameLight = 'light-mode';
 
-  console.log(theme)
-
   function setClassOnDocumentBody(darkMode) {
     document.body.classList.add(darkMode ? classNameDark : classNameLight);
     document.body.classList.remove(darkMode ? classNameLight : classNameDark);
